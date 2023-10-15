95484ea6ba61223b08dce53b7f7b41861ed1706f
bobby gintz <bobby.gintz@radpowerbikes.com>
2023-10-15 13:50:19 -0400

    Initial Commit

---
+++ vesc-emulator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  vesc-emulator
+//
+//  Created by Robert Glntz on 10/15/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
