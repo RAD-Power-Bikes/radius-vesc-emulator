a267f4fb4d89098a2b52acb78363c317972424fe
bobby gintz <bobby.gintz@radpowerbikes.com>
2023-10-18 16:12:04 -0400

    WIP:  added files required to support basic commands required on VESC connect:  COMM_FW_VERSION,  COMM_GET_MC_CONF, COMM_GET_APPCONF

--- vesc-emulator/vesc_interface.h
+++ vesc-emulator/vesc_interface.h
@@ -12,13 +12,18 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-#define HW_NAME "RADIUS"
+#include "mempools.h"
+#include "conf_general.h"
+#include "appconf_default.h"
+#include "appconf_custom.h"
 
-// Firmware version
-#define FW_VERSION_MAJOR            5
-#define FW_VERSION_MINOR            03
-// Set to 0 for building a release and iterate during beta test builds
-#define FW_TEST_VERSION_NUMBER        0
+//#define HW_NAME "RADIUS"
+//
+//// Firmware version
+//#define FW_VERSION_MAJOR            5
+//#define FW_VERSION_MINOR            03
+//// Set to 0 for building a release and iterate during beta test builds
+//#define FW_TEST_VERSION_NUMBER        0
 #define STM32_UUID_8                0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40  // required for build.  Identifies MCU type to VESC client
 
 // chbios entities converted to c
